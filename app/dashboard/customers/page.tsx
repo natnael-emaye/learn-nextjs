f2d625595d740ca80933f2d9022aa117da7e1152
natnael-emaye <natnaelabebegualu@gmail.com>
2024-06-18 12:54:25 +0300

    create nested route

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function page() {
+  return <h1 className="text-2xl">customers page</h1>
+};
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function page() {
+  return <h1 className="text-2xl">Invoices page</h1>
+};
